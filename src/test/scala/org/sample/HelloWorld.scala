4005bf26aef2db8092770f18e71f21f544ede126
Loïc Knuchel <l.knuchel@criteo.com>
2017-05-05 22:30:29 +0200

    improve lol integration

--- src/test/scala/org/sample/HelloWorld.scala
+++ src/test/scala/org/sample/HelloWorld.scala
@@ -1,13 +1,22 @@
 package org.sample
 
+import io.circe.Json
+import io.circe.optics.JsonPath._
 import lol.http._
+import lol.json._
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
+import org.scalatest.time.{Millis, Seconds, Span}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
-class HelloWorld extends FunSpec with Matchers with ScalaFutures {
+class HelloWorld extends FunSpec with Matchers with ScalaFutures with BeforeAndAfterAll {
+  implicit val defaultPatience = PatienceConfig(timeout = Span(5, Seconds), interval = Span(500, Millis))
+  val typicodeClient = Client("jsonplaceholder.typicode.com", 443, "https")
+
+  override def afterAll(): Unit = {
+    typicodeClient.stop()
+  }
 
   describe("Hello World") {
     it("should say Hello") {
@@ -15,17 +24,11 @@ class HelloWorld extends FunSpec with Matchers with ScalaFutures {
       hello("World") shouldBe "Hello World"
     }
 
-    // GET https://jsonplaceholder.typicode.com/posts/1
     it("should perform requests") {
-      case class Post(userId: Int, id: Int, title: String, body: String)
-      Client("jsonplaceholder.typicode.com", 443, maxConnections = 1).runAndStop { client =>
-        for {
-          r <- client.run(Get("/posts/1"))(Future.successful)
-          _ = r.status shouldBe 200
-        } yield ()
+      val res = typicodeClient.run(Get("/users")) { response =>
+        response.readAs[Json].map(root.each.username.string.getAll)
       }
-      //sampleClient(Get("/posts/1")).futureValue.status shouldBe 200
+      res.futureValue shouldBe List()
     }
   }
-
 }
